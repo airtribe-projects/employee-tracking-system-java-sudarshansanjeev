6fcdf66927c711fcf49989adc240d0e39cdd4f9e
sudarshan <sudarshansanjeev389@gmail.com>
2025-03-10 13:21:55 +0530

    added exception to for empty names in employee

--- employeeTrackingSystem/src/main/java/org/airtribe/controller/EmployeeController.java
+++ employeeTrackingSystem/src/main/java/org/airtribe/controller/EmployeeController.java
@@ -3,6 +3,8 @@ package org.airtribe.controller;
 import org.airtribe.model.Employee;
 import org.airtribe.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +18,16 @@ public class EmployeeController {
     EmployeeService employeeService;
 
     @PostMapping("/createEmployee")
-    public Employee createEmployee(@RequestBody Employee employee) {
-        return employeeService.createEmployee(employee);
+    public ResponseEntity<?> createEmployee(@RequestBody Employee employee) {
+        try {
+            var employeeCreated = employeeService.createEmployee(employee);
+            return ResponseEntity.status(HttpStatus.CREATED).body(employeeCreated);
+        }
+        catch (IllegalArgumentException ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+        }
+        catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+        }
     }
 }
--- employeeTrackingSystem/src/main/java/org/airtribe/service/EmployeeService.java
+++ employeeTrackingSystem/src/main/java/org/airtribe/service/EmployeeService.java
@@ -5,13 +5,17 @@ import org.airtribe.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import static org.apache.logging.log4j.util.Strings.isEmpty;
+
 @Service
 public class EmployeeService {
     @Autowired
     EmployeeRepository employeeRepository;
 
-    public Employee createEmployee(Employee employee) {
-        // add repository layer code;
+    public Employee createEmployee(Employee employee) throws Exception{
+        if (isEmpty(employee.getName())) {
+            throw new IllegalArgumentException("Name is mandatory");
+        }
 
         employeeRepository.save(employee);
         //employeeRepository.findById(employee.getId());
